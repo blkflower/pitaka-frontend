d65c0332c4c2598e8936cc9662b8e827166166a6
Japheth Balane <japheth162@gmail.com>
2023-10-25 22:06:45 +0800

    Added login success state

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -2,35 +2,36 @@
 	import type { AlertType } from "$lib/types";
 
   export let type: AlertType = 'info';
-  export let label: string;
 
   $: borderStyle = () => {
     switch (type) {
       case 'info':
-        return 'border-blue-500 bg-blue-50';
+        return 'border-blue-700 bg-blue-50';
       case 'success':
-        return 'border-green-500 bg-green-50';
+        return 'border-green-700 bg-green-50';
       case 'warning':
-        return 'border-yellow-500 bg-yellow-50';
+        return 'border-yellow-700 bg-yellow-50';
       case 'error':
-        return 'border-red-500 bg-red-50';
+        return 'border-red-700 bg-red-50';
     }
   };
 
   $: labelStyle = () => {
     switch (type) {
       case 'info':
-        return 'text-blue-500';
+        return 'text-blue-700';
       case 'success':
-        return 'text-green-500';
+        return 'text-green-700';
       case 'warning':
-        return 'text-yellow-500';
+        return 'text-yellow-700';
       case 'error':
-        return 'text-red-500';
+        return 'text-red-700';
     }
   };
 </script>
 
-<div class={`p-2 px-4 rounded border ${$$props.class} ${borderStyle()}`}>
-  <p class={`text-sm ${labelStyle()}`}>{label}</p>
+<div class={`p-2 px-4 rounded border border-2 ${$$props.class} ${borderStyle()}`}>
+  <p class={`text-sm ${labelStyle()}`}>
+    <slot />
+  </p>
 </div>
